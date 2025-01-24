8cc61e6e259482f0d83857498b6fa8467812b58d
adanisantana <mariano.dani@hotmail.com>
2025-01-24 17:39:26 -0300

    alteracoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,9 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
-import Footer from "./componentes/footer/Footer"
-import Home from "./pages/home/Home"
-import Navbar from "./componentes/navbar/Navbar"
-import Cadastro from "./pages/cadastro/Cadastro"
-import Login from "./pages/login/Login"
-import { AuthProvider } from "./contexts/AuthContext"
-import ListaTemas from "./componentes/temas/listatemas/ListaTemas"
-import FormTema from "./componentes/temas/formtema/FormTema"
-import DeletarTema from "./componentes/temas/deletartema/DeletarTema"
-import ListaPostagens from "./componentes/postagens/listapostagens/ListaPostagens"
-import FormPostagem from "./componentes/postagens/formpostagem/FormPostagem"
-import DeletarPostagem from "./componentes/postagens/deletarpostagem/DeletarPostagem"
-import Perfil from "./pages/perfil/Perfil"
-import { ToastContainer } from "react-toastify"
-
+import Home from "./home/Home"
 
 
 function App() {
     return (
-       <>
-       <AuthProvider>
-        <ToastContainer/>
-        <BrowserRouter>
-        <Navbar/>
-        <div className="min-h-[80vh]">
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />}/>
-                <Route path="/cadastro" element={<Cadastro/>}/>
-                <Route path="/login" element={<Login />} />
-                <Route path="/temas" element={<ListaTemas />} />
-                <Route path="/cadastrartema" element={<FormTema />} />
-                <Route path="/editartema/:id" element={<FormTema />} />
-                <Route path="/deletartema/:id" element={<DeletarTema />} />
-                <Route path="/postagens" element={<ListaPostagens />} />
-                <Route path="/cadastrarpostagem" element={<FormPostagem />} />
-                <Route path="/editarpostagem/:id" element={<FormPostagem />} />
-                <Route path="/deletarpostagem/:id" element={<DeletarPostagem/>}/>
-                <Route path="/perfil" element={<Perfil />} />
-            </Routes>
-        </div>
-        <Footer/>
-        </BrowserRouter>
-        </AuthProvider>
-       </>
+        <Home/>
     )
 }
 
---
+++ src/home/Home.tsx
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <>
+            <div className="w-screen flex justify-center  bg-white ">
+                <div>
+                    <div className="max-w-7xl flex flex-col items-center ">
+                        <p className="text-blue-400 font-sans font-medium m-4">
+                            Encontre desenvolvedores que compartilham o que sabem</p>
+                    </div>
+
+                    <div className="max-w-7xl flex flex-col items-center  ">
+                        <img className='rounded-lg ...'
+                            src="./src/assets/banner.png"
+                            alt="Imagem da Página Home"
+                            width="1200px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
